c039a15ebfc1ad5bdcf05a27a43b4f63f1d3398d
Matthieu96Code <117777066+Matthieu96Code@users.noreply.github.com>
2023-07-19 15:49:45 +0000

    Fix linters error

--- test/controllers/messages_controller_test.rb
+++ test/controllers/messages_controller_test.rb
@@ -1,7 +1,7 @@
-require "test_helper"
+require 'test_helper'
 
 class MessagesControllerTest < ActionDispatch::IntegrationTest
-  test "should get greeting" do
+  test 'should get greeting' do
     get messages_greeting_url
     assert_response :success
   end
